aa8c7012b2780ab895a3a7d60f0380923b74a20e
Ashfaqzaheer <Ashfaqzaheer18@gmail.com>
2026-01-15 02:04:54 -0800

    add disk usage monitoring shell script

---
+++ disk_check.sh
@@ -0,0 +1,9 @@
+THRESHOLD=80
+USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
+
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+	echo "WARNING: Disk usage is above ${THRESHOLD}%"
+else
+
+	echo "Disk usage is under control"
+fi
---
+++ userinfo.sh
@@ -0,0 +1,5 @@
+echo "Enter your name:"
+read Name
+
+echo "Hello $NAME"
+echo "You are learning Devops on $(date)"
